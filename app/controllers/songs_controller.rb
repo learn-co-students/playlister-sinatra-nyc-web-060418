00dc61b5e42b5eaa7a78ef7f9ece42298dace37c
Ms-Alex <ms.hernandeza1@gmail.com>
2018-06-28 18:25:54 -0400

    used Artist.find_or_create_by in songs controller on post /songs

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -25,15 +25,15 @@ class SongsController < Sinatra::Base
   end
 
   post '/songs' do
+    @song = Song.create(name: params[:song][:name])
 
+    @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
 
-    @song = Song.create(name: params[:name])
-
-    if !Artist.all.find_by(name: params[:artist][:name])
-      @song.artist = Artist.create(name: params[:artist][:name])
-      @song.artist.save
-      #binding.pry
-    end
+    # if !Artist.all.find_by(name: params[:artist][:name])
+    #   @song.artist = Artist.create(name: params[:artist][:name])
+    #   @song.artist.save
+    #   #binding.pry
+    # end
 
     redirect "/songs/#{@song.slug}"
   end
